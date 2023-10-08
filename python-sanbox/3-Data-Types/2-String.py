a4d4b794fee4a05be88f10050fd199bf0a2cabef
pratham rai <97867973+pratham5368@users.noreply.github.com>
2023-10-08 21:25:26 +0530

    Update 2-String.py
    added few new things in code

--- python-sanbox/3-Data-Types/2-String.py
+++ python-sanbox/3-Data-Types/2-String.py
@@ -584,3 +584,8 @@ print("expensive" not in txt)
 txt = "The best things in life are free!"
 if "expensive" not in txt:
   print("No, 'expensive' is NOT present.")
+
+# reverse a string:-There is no built-in function to reverse a String in Python.
+#The fastest (and easiest?) way is to use a slice that steps backwards, -1.
+txt = "Hello World"[::-1]
+print(txt)
